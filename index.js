1c9902d326b31a9b85b9f733a264b8ecda14bc38
olegbucolo <olegbucolo97@gmail.com>
2025-12-15 12:39:36 +0100

    calcolato il peso

--- index.js
+++ index.js
@@ -1,72 +1,81 @@
-class Zucchine{
+class Zucchine {
+
+    *[Symbol.iterator]() {
+        for (const key in this) {
+            yield this[key];
+        }
+    }
+
     zucchina1 = {
         varieta: "var1",
-        peso: "weight1",
+        peso: 1,
         lunghezza: "length1"
     }
 
     zucchina2 = {
         varieta: "var2",
-        peso: "weight2",
+        peso: 12,
         lunghezza: "length2"
     }
 
     zucchina3 = {
         varieta: "var3",
-        peso: "weight3",
+        peso: 13,
         lunghezza: "length3"
     }
 
     zucchina4 = {
         varieta: "var4",
-        peso: "weight4",
+        peso: 14,
         lunghezza: "length4"
     }
 
     zucchina5 = {
         varieta: "var5",
-        peso: "weight5",
+        peso: 15,
         lunghezza: "length5"
     }
 
     zucchina6 = {
         varieta: "var6",
-        peso: "weight6",
+        peso: 16,
         lunghezza: "length6"
     }
 
     zucchina7 = {
         varieta: "var7",
-        peso: "weight7",
+        peso: 17,
         lunghezza: "length7"
     }
 
     zucchina8 = {
         varieta: "var8",
-        peso: "weight8",
+        peso: 1,
         lunghezza: "length8"
     }
 
     zucchina9 = {
         varieta: "var9",
-        peso: "weight9",
+        peso: 18,
         lunghezza: "length9"
     }
 
     zucchina10 = {
         varieta: "var10",
-        peso: "weight10",
+        peso: 19,
         lunghezza: "length10"
     }
 }
-
+let pesoTot = 0;
 const zucchine = new Zucchine();
-for(let zucchina in zucchine){
+for (let zucchina in zucchine) {
 
     console.log('Nome zucchina', zucchina)
-    console.log(`varieta'`, zucchine[zucchina].varieta)
-    console.log(`Peso`, zucchine[zucchina].peso)
-    console.log(`Lunghezza `, zucchine[zucchina].lunghezza)
+    console.log(`varieta':`, zucchine[zucchina].varieta)
+    console.log(`Peso:`, zucchine[zucchina].peso)
+    pesoTot += zucchine[zucchina].peso
+    console.log(`Lunghezza:`, zucchine[zucchina].lunghezza)
+    console.log(`PESO TOTALE = `, pesoTot)
+    
     console.log('______________')
-
 }
